4a6e389e23e14be665bc07637701601564015e12
uluj <gungoruluc@gmail.com>
2025-05-27 20:51:06 +0300

    lava and door

--- Assets/Abandoned_Asylum/Animations/LavaAnimation/LavaAnimation.cs
+++ Assets/Abandoned_Asylum/Animations/LavaAnimation/LavaAnimation.cs
@@ -1,18 +1,42 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LavaAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public string triggeringTag = "Player"; // Tag to detect
+
+    private Animator animator;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        if (animator != null)
+        {
+            animator.enabled = false; // Start disabled, will be enabled on trigger
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Trigger-based interaction (e.g., when entering a zone)
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag(triggeringTag))
+        {
+            if (animator != null)
+            {
+                animator.enabled = true;
+            }
+        }
+    }
+
+    // Collision-based interaction (e.g., hitting the object physically)
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag(triggeringTag))
+        {
+            // Restart the game by reloading the active scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 }
--- Assets/Abandoned_Asylum/kitapAnimasyon/keyKodu/Anahtar.cs
+++ Assets/Abandoned_Asylum/kitapAnimasyon/keyKodu/Anahtar.cs
@@ -4,15 +4,16 @@ using UnityEngine;
 
 public class Anahtar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
+    public Material targetMaterial;         // The material to change
+    public string triggeringTag = "Player"; // Tag that triggers the change
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag(triggeringTag) && targetMaterial != null)
+        {
+            targetMaterial.color = Color.green;
+            gameObject.SetActive(false);
+        }
     }
 }
